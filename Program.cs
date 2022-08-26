ae3ec33a8460e96e0bfc23a6ffb77674ff49fa39
bsralkan <bsr.alkan@gmail.com>
2022-08-26 11:45:47 +0300

    added request for getting token

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿using NessusApis;
 using System;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            HttpResponseMessage data;
+            HttpContent content;
+            string response;
+
             //Blocked SSL errors
             HttpClientHandler clientHandler = new HttpClientHandler();
             clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
@@ -22,6 +29,17 @@ namespace ConsoleApp4
             var password = Console.ReadLine();
 
             UserInfo userInfo = new UserInfo(username, password);
+
+            var json = JsonSerializer.Serialize(userInfo);
+            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            httpClient.BaseAddress = new Uri(url);
+
+            data = await httpClient.PostAsync("session", stringContent);
+            content = data.Content;
+            response = await content.ReadAsStringAsync();
+
+            Console.WriteLine(response);
         }
     }
 }
