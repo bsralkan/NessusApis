ff1dc4c784a67f7d0bf4a5282d7cf8affeea6e69
bsralkan <bsr.alkan@gmail.com>
2022-08-26 11:27:07 +0300

    Add project files.

---
+++ UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NessusApis
+{
+    class UserInfo
+    {
+        private string _username;
+        private string _password;
+
+        public UserInfo(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        public string username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+    }
+}
